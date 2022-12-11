293ebd5e65c0665e9234b4fdbfedf6cc8724ebaa
vipulgole31 <120101845+vipulgole31@users.noreply.github.com>
2022-12-11 12:47:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 "use strict";
 const time = document.querySelector('.time');
 const refDate = new Date('08-12-2027');
-time.textContent = Math.abs(Math.floor((Date.now() - refDate)/(1000*60*60*24)));
+
+time.innerHTML = `Starting at 08-12-2022 
+ ${Math.abs(Math.floor((Date.now() - refDate) / (1000 * 60 * 60 * 24)))} days`;
